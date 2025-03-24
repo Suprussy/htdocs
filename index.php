5a4be0d1b6ffbf4211e58cfb17818b43c227152d
Suprussy <suprussy73@gmail.com>
2025-03-24 14:54:30 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WEB</title>
+</head>
+
+<body>
+  <h1>WEB</h1>
+  <ol>
+    <li><a href="index.php?id=HTML">HTML</a></li>
+    <li><a href="index.php?id=CSS">CSS</a></li>
+    <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>
+    <?= $_GET['id'] ?>
+  </h2>
+  <?= file_get_contents("data/" . $_GET['id']) ?>
+</body>
+
+</html>
