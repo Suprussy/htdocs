1f5324b2d40af2ff3833653785650d3bf194ba23
Suprussy <suprussy73@gmail.com>
2025-03-24 18:10:02 +0900

    오타 수정

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ function print_list()
   <a href="create.php">create</a>
   <?php if (isset($_GET['id'])) { ?>
     <a href="update.php?id=<?= $_GET['id'] ?>">update</a>
-    <form action="delete_procss.php" method="post">
+    <form action="delete_process.php" method="post">
       <input type="hidden" name="id" value="<?= $_GET['id'] ?>" />
       <input type="submit" value="delete" />
     </form>
