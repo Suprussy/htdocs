3fb03122a6463b6ae2d0e4148acaafc1c4b3241a
Suprussy <suprussy73@gmail.com>
2025-03-24 15:52:21 +0900

    'print_description()', 'print_list()' 함수 생성

--- index.php
+++ index.php
@@ -9,6 +9,31 @@ function print_title()
 }
 ?>
 
+<?php
+function print_description()
+{
+  if (isset($_GET['id'])) {
+    echo file_get_contents("data/" . $_GET['id']);
+  } else {
+    echo "Hello, PHP";
+  }
+}
+?>
+
+<?php
+function print_list()
+{
+  $list = scandir('./data');
+  $i = 0;
+  while ($i < count($list)) {
+    if ($list[$i] != '.' && $list[$i] != '..') {
+      echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</li>\n";
+    }
+    $i = $i + 1;
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,25 +48,10 @@ function print_title()
     <?php print_title() ?>
   </h1>
   <ol>
-    <?php
-    $list = scandir('./data');
-    $i = 0;
-    while ($i < count($list)) {
-      if ($list[$i] != '.' && $list[$i] != '..') {
-        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</li>\n";
-      }
-      $i = $i + 1;
-    }
-    ?>
+    <?php print_list() ?>
   </ol>
   <h2><?php print_title() ?></h2>
-  <?php
-  if (isset($_GET['id'])) {
-    echo file_get_contents("data/" . $_GET['id']);
-  } else {
-    echo "Hello, PHP";
-  }
-  ?>
+  <?php print_description() ?>
 </body>
 
 </html>
