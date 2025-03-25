019a2566d00bcd41263e56b913ae1a9a584a3234
Suprussy <suprussy73@gmail.com>
2025-03-25 22:18:16 +0900

    'insert.php' 생성

--- mysql/insert.php
+++ mysql/insert.php
@@ -1,14 +1,19 @@
 <?php
-// $mysqli = mysqli_connect("example.com", "user", "password", "database");
-// $result = mysqli_query($mysqli, "SELECT 'Please do not use the deprecated mysql extension for new development. ' AS _msg FROM DUAL");
-// $row = mysqli_fetch_assoc($result);
-// echo $row['_msg'];
+$user = "root";
+$pass = "root";
+$host = "127.0.0.1"; // 포트는 따로 지정할 것
+$port = 8889; // MAMP MySQL 포트
+$db = "opentutorials"; // 연결할 DB 없으면 비워도 됨
 
-$conn = mysqli_connect("localhost", "root", "12345678", "opentutorials");
+$conn = new mysqli($host, $user, $pass, $db, $port);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 mysqli_query($conn, "
   insert into topic (
     title, description, created
   ) values (
-    'MySQL', 'MySQL is ....', NOW() 
+    'MySQL', 'MySQL is ...', now()
   )
 ");
