89a57e881ef29ed7e899ac11b548f93dcaf0491c
Suprussy <suprussy73@gmail.com>
2025-03-25 14:25:08 +0900

    mysql 'index.php' 파일 생성

---
+++ mysql/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WEB</title>
+</head>
+
+<body>
+  <h1>WEB</h1>
+  <ol>
+    <li>HTML</li>
+  </ol>
+  <h2>Welcome</h2>
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure libero omnis incidunt sapiente rerum exercitationem in, autem minus reiciendis? Dolorum ex exercitationem vitae qui est beatae rem. Dolores, amet corporis.
+</body>
+
+</html>
