864be7228c2911eebad67267ff51331dbbb272cb
Suprussy <suprussy73@gmail.com>
2025-03-26 15:14:11 +0900

    뭘 바꾼지 모름

--- mysql/index.php
+++ mysql/index.php
@@ -11,14 +11,15 @@ $result = mysqli_query($conn, $sql);
 $list = '';
 
 while ($row = mysqli_fetch_array($result)) {
-  $list = $list . "<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>"; // 기존 $list에 누적
+  $escaped_title = htmlspecialchars($row['title']);
+  $list = $list . "<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>"; // 기존 $list에 누적
 }
 if (isset($_GET['id'])) {
   $sql = "select * from topic where id={$_GET['id']}";
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_array($result);
-  $article['title'] = $row['title'];
-  $article['description'] = $row['description'];
+  $article['title'] = htmlspecialchars($row['title']);
+  $article['description'] = htmlspecialchars($row['description']);
 }
 ?>
 
