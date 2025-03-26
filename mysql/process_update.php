13fba1ec4ca5d0f51572e7828aa3374eee3c3c9c
Suprussy <suprussy73@gmail.com>
2025-03-26 17:27:40 +0900

    수정 기능 삽입

--- mysql/process_update.php
+++ mysql/process_update.php
@@ -4,25 +4,26 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // 예외 모드 활�
 try {
   $conn = new mysqli("127.0.0.1", "root", "root", "opentutorials", 8889);
 
+  settype($_POST['id'], 'integer');
   $filtered = array(
     'title' => mysqli_real_escape_string($conn, $_POST['title']),
-    'description' => mysqli_real_escape_string($conn, $_POST['description'])
+    'description' => mysqli_real_escape_string($conn, $_POST['description']),
+    'id' => $_POST['id']
   );
 
   $sql = "
-    insert into topic (
-      title, description, created
-    ) values (
-      '{$filtered['title']}',
-      '{$filtered['description']}',
-      now() 
-    )
+    UPDATE topic
+    SET
+      title = '{$filtered['title']}',
+      description = '{$filtered['description']}'
+    WHERE
+      id = '{$filtered['id']}'
   ";
 
   $result = mysqli_query($conn, $sql);
 
   echo '성공했습니다. <a href="index.php">돌아가기</a>';
 } catch (mysqli_sql_exception $e) {
-  echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
+  echo '수정하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
   error_log($e->getMessage()); // 서버 에러 로그에 상세 내용 기록
 }
--- mysql/update.php
+++ mysql/update.php
@@ -39,7 +39,8 @@ if (isset($_GET['id'])) {
 <body>
   <h1><a href="index.php">WEB</a></h1>
   <ol><?= $list ?></ol>
-  <form action="process_create.php" method="POST">
+  <form action="process_update.php" method="POST">
+    <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
     <p><input type="text" name="title" placeholder="title" value="<?= $article['title'] ?>"></p>
     <p><textarea name="description" placeholder="description"><?= $article['description'] ?></textarea></p>
     <p><input type="submit"></p>
